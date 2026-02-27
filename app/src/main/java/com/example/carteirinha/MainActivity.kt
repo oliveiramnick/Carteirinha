2c886c7655d32b3b1e3688c90437773feb95e323
Nicoly de Oliveira Machado <nicoly.machado04@gmail.com>
2026-02-27 11:01:13 -0300

    27/02/2026

--- app/src/main/java/com/example/carteirinha/MainActivity.kt
+++ app/src/main/java/com/example/carteirinha/MainActivity.kt
@@ -5,22 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,72 +59,96 @@ class MainActivity : ComponentActivity() {
 @Composable
 
 fun CarteirinhaDeEstudante(modifier: Modifier = Modifier) {
-
-    Column(modifier = modifier) {
+    Box(){
         Image(
-
-            painter = painterResource(id = R.drawable.img),
-
+            painter = painterResource( R.drawable.wallpaper),
             contentDescription = null,
-
-            modifier = Modifier.size(200.dp)
-
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row {
-
-            Text(
-
-                text = "Nome: ",
-
-                fontSize = 25.sp,
-
-                style = MaterialTheme.typography.bodyMedium
+        Column(modifier = modifier
+            .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp))
+        {
+            Image(
+                painter = painterResource( R.drawable.img),
+                contentDescription = null,
+                modifier = Modifier.
+                fillMaxWidth(.6f)
+                    .background(color = Color.Blue)
 
             )
+            Spacer(modifier = Modifier.weight(.3f)
+            )
+            Image(
+                painter = painterResource( R.drawable.avatar),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.
+                weight(2f)
+                    .fillMaxSize(.6f)
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
 
-            Text(
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-                text = "Nicoly Machado",
+            Row (
+                modifier = Modifier
+                    .weight(2f)
+                    .fillMaxSize(.9f),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
 
-                fontSize = 25.sp,
+            ){
+                Text(
+                    text="Nome: ",
+                    style= MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Right,
+                    modifier = Modifier
+                        .weight(1f)
+                )
+                Text(
+                    text = "Nicoly Machado",
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    modifier = Modifier.weight(3f),
+                    fontFamily = MaterialTheme.typography.titleLarge.fontFamily,
+                    color = MaterialTheme.colorScheme.primary
+                )
 
-                style = MaterialTheme.typography.bodyLarge
+            }
 
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row {
+
+                Text(
+
+                    text = "Curso: ",
+
+                    fontSize = 20.sp,
+
+                    style = MaterialTheme.typography.bodyMedium
+
+                )
+
+                Text(
+
+                    text = "Desenvolvimento de Sistemas",
+
+                    fontSize = 20.sp,
+
+                    style = MaterialTheme.typography.bodyLarge
+
+                )
+
+            }
+            QrCode(
+                conteudo = "24247634"
             )
 
         }
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Row {
-
-            Text(
-
-                text = "Curso: ",
-
-                fontSize = 20.sp,
-
-                style = MaterialTheme.typography.bodyMedium
-
-            )
-
-            Text(
-
-                text = "Desenvolvimento de Sistemas",
-
-                fontSize = 20.sp,
-
-                style = MaterialTheme.typography.bodyLarge
-
-            )
-
-        }
-        QrCode(
-            conteudo = "4242655"
-        )
 
     }
 
--- carteirinha/app/src/main/java/com/example/carteirinha/ui/theme/Type.kt
+++ carteirinha/app/src/main/java/com/example/carteirinha/ui/theme/Type.kt
@@ -34,29 +34,17 @@ val displayFontFamily = FontFamily(
 val baseline = Typography()
 
 val AppTypography = Typography(
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Cursive,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 30.sp
-    ),
     displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
     displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
     displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
     headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
     headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
     headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
     titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
     titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
     bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
     labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
     labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
