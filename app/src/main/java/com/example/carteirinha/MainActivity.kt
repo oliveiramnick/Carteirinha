aed3fbaa6e5e7770c719547d19ec7022a68c14fa
Nicoly de Oliveira Machado <nicoly.machado04@gmail.com>
2026-02-27 11:02:22 -0300

    27/02/2026

--- app/src/main/java/com/example/carteirinha/MainActivity.kt
+++ app/src/main/java/com/example/carteirinha/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -64,7 +65,9 @@ fun CarteirinhaDeEstudante(modifier: Modifier = Modifier) {
             painter = painterResource( R.drawable.wallpaper),
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .alpha(.3f)
         )
         Column(modifier = modifier
             .fillMaxSize(),
